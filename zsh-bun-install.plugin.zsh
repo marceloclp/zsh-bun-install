b4d92627a4a9f2adcde6434236fe183461bb1752
Marcelo Perrella <marceloclperrella@gmail.com>
2025-01-08 09:23:05 +0000

    refactor: remove unnecessary fns

--- zsh-bun-install.plugin.zsh
+++ zsh-bun-install.plugin.zsh
@@ -6,34 +6,21 @@
     [[ ":$PATH:" != *":$1:"* ]] && export PATH="$1:$PATH"
   }
 
-  # Initializes and exports a variable with a default value if unset:
-  # @usage   init_env <env_name> <default_value>
-  # @example init_env BUN_DIR    $HOME/.bun
-  function init_env() {
-    [[ -z "${(P)1}" ]] && export $1="$2"
-  }
-
+  # Checks if a command is installed
   # @usage   is_installed <command_name>
   # @example is_installed bun
   function is_installed() {
     command -v "$1" >/dev/null 2>&1
   }
 
-  # Checks if a directory exists:
-  # @usage   is_dir <path>
-  # @example is_dir $HOME/.bun
-  function is_dir() {
-    [[ -d "$1" ]]
-  }
-
   # Bun installation directory:
-  init_env BUN_DIR $HOME/.bun
+  export BUN_DIR=${BUN_DIR:-$HOME/.bun}
   # Whether to enable bun completion:
-  init_env BUN_ENABLE_COMPLETION true
+  export BUN_ENABLE_COMPLETION=${BUN_ENABLE_COMPLETION:-true}
 
   # @see https://bun.sh/docs/installation
   function install_bun() {
-    if ! is_dir $BUN_DIR; then
+    if ! [[ -d "$BUN_DIR" ]]; then
       curl -fsSL https://bun.sh/install | BUN_INSTALL=$BUN_DIR IS_BUN_AUTO_UPDATE=false bash
     fi
 
@@ -48,7 +35,7 @@
     if ! $BUN_ENABLE_COMPLETION; then return; fi
 
     # Ensure the cache directory exists:
-    if ! is_dir $ZSH_CACHE_DIR/completions; then
+    if ! [[ -d "$ZSH_CACHE_DIR/completions" ]]; then
       mkdir -p $ZSH_CACHE_DIR/completions
     fi
 
